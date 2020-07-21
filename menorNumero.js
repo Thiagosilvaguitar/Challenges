17d19b8db126b4c32ee6668663bfa6e616a42cd4
Thiagosilvaguitar <thiagosilva_guitar@hotmail.com>
2020-07-21 19:15:15 -0300

    0

---
+++ menorNumero.js
@@ -0,0 +1,14 @@
+/*Funções parametrizadas - Menor Número
+Escreva uma função chamada menorNumero. Ela deve receber dois números como parâmetro e retornar o menor entre eles. Caso os números sejam iguais, basta que se retorne qualquer um deles.*/
+
+function menorNumero(a, b){
+    a = 5
+    b = 10
+    if(a < b){
+        return a
+    }else{
+        return b
+    }
+}
+    
+console.log(menorNumero)
