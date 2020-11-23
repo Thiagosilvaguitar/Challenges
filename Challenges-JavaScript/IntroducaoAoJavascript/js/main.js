845cdc26446f352bcf7806e7d0f4b2ac8a560f64
Thiago A. Silva <57600875+Thiagosilvaguitar@users.noreply.github.com>
2020-11-23 15:24:36 -0300

    Add files via upload

---
+++ Challenges-JavaScript/IntroducaoAoJavascript/js/main.js
@@ -0,0 +1,25 @@
+// var nome = "Thiago Antonio"
+// var idade = 24
+// var idade2 = 25
+// alert(nome + " tem " + idade + " Anos")
+// console.log(idade + idade2)
+// var frase = "Japão é o melhor time do mundo"
+// console.log(frase.toLowerCase())
+
+// var lista = [ "maca", "pera" , "laranja"]
+// lista.push("uva")
+// lista.pop()
+// console.log(lista.reverse())
+// console.log(lista.length)
+// console.log(lista.toString()[0])
+// console.log(lista.join("-"))
+// console.log(lista.join("-"))
+
+// var frutas = [{nome:"maca", cor:"Vermelha"},{nome:"Uva", cor:"roxa"}]
+// console.log(frutas)
+// alert(frutas[1].nome)
+
+var d = new Date()
+alert(d.getMinutes())
+
+
